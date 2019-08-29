ffd85f2d5f1ba4fb09c984504419106da42a5e21
alicebyers5 <alice.byers@nhs.net>
2019-08-29 11:17:26 +0100

    Add purrr to package list

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -30,6 +30,7 @@ library(glue)          # For working with strings
 library(ggplot2)       # For plotting
 library(plotly)        # For interactive plots
 library(flexdashboard) # For creating markdown outputs
+library(purrr)         # For functional programming
 
 
 ### 2 - Define Whether Running on Server or Locally ----
