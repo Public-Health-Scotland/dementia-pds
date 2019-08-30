601b9c2190788f35558899264f602c4d870a923a
alicebyers5 <alice.byers@nhs.net>
2019-08-30 08:42:36 +0100

    Complete data with no referrals. Fix issue #11.

--- functions/plot_referrals.R
+++ functions/plot_referrals.R
@@ -19,26 +19,38 @@ plot_referrals <- function(data, scotland = FALSE){
         group_by(fy, month, health_board, ijb) %>%
         summarise(referrals = sum(referrals))
     
-    data %<>% bind_rows(board)
+    data %<>% 
+      bind_rows(board) %>% 
+      ungroup() %>%
+      complete(month, ijb,
+               fill = list(fy = max(.$fy),
+                           health_board = max(.$health_board),
+                           referrals = 0))
     
   }else{
     
     data %<>% 
-      mutate(health_board = ifelse(scot == TRUE, "Scotland", health_board)) %>%
+      mutate(health_board = ifelse(scotland == TRUE, "Scotland", health_board)) %>%
       group_by(fy, month, health_board) %>%
-      summarise(referrals = sum(referrals))
+      summarise(referrals = sum(referrals)) %>%
+      ungroup() %>%
+      complete(month, health_board,
+               fill = list(fy = max(.$fy),
+                           referrals = 0))
   
   }
   
   data %<>%
-    
+
     mutate(year = if_else(month %in% 1:3,
                           paste0(substr(fy, 1, 2), substr(fy, 6, 7)),
                           substr(fy, 1, 4)),
            month_full = month(month, label = TRUE, abbr = FALSE),
            month_abbr = forcats::fct_relevel(
              month(month, label = TRUE),
-             "Jan", "Feb", "Mar", after = Inf)) %>%
+             "Jan", "Feb", "Mar", after = Inf))
+  
+  data %>%
     
     ggplot(aes(x = month_abbr,
                y = referrals,
