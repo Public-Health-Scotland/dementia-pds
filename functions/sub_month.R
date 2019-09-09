c7ada1229b3704ac0cf0758492cd2044d5448377
alicebyers5 <alice.byers@nhs.net>
2019-09-09 17:15:23 +0100

    Add submission date to home page.

---
+++ functions/sub_month.R
@@ -0,0 +1,20 @@
+
+sub_month <- function(fy, qt){
+  
+  month <- 
+    case_when(
+      qt == 1 ~ "July",
+      qt == 2 ~ "October",
+      qt == 3 ~ "January",
+      qt == 4 ~ "April"
+    )
+  
+  year <- 
+    case_when(
+      qt %in% 1:2 ~ substr(sel_fy, 1, 4),
+      qt %in% 3:4 ~ paste0("20", substr(sel_fy, 6, 7))
+    )
+  
+  paste(month, year)
+  
+}
