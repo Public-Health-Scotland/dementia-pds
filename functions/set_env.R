bc63606a6992a868d89b1f3dca5e286bdccde764
alicebyers5 <alice.byers@nhs.net>
2019-09-09 17:33:53 +0100

    Add Scotland data for HB pages

--- functions/set_env.R
+++ functions/set_env.R
@@ -18,10 +18,19 @@ set_env <- function(data = pds,
       filter(fy == year & health_board == hb)
   }
   
+  # Get Scotland data for health board benchmarking
+  if(hb != "Scotland"){
+    subpage_data_scot <-
+      data %>%
+      filter(fy == year)
+  }
+  
   # Get expected diagnoses figure for selected Health Board and FY
   subpage_exp  <- exp %>%
     filter(health_board_label == hb & fy == year) %>%
     pull(diagnoses)
+  
+  subpage_exp_scot <- sum(exp$diagnoses)
   
   # Assign objects to subpage environment
   assign("subpage_data", subpage_data, subpage_env)
@@ -31,6 +40,11 @@ set_env <- function(data = pds,
   assign("all_fy", setdiff(unique(data$fy), year), subpage_env)
   assign("exp", subpage_exp, subpage_env)
   assign("qt", quarter, subpage_env)
+  
+  if(hb != "Scotland"){
+    assign("subpage_data_scot", subpage_data_scot, subpage_env)
+    assign("exp_scot", subpage_exp_scot, subpage_env)
+  }
   
   return(
     if(hb == "Scotland"){
