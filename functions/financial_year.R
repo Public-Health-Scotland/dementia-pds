98d1f2a297d421873d8780ba2da5645f91699fe5
alicebyers5 <alice.byers@nhs.net>
2019-09-12 08:58:54 +0100

    Add missing bracket

--- functions/financial_year.R
+++ functions/financial_year.R
@@ -3,6 +3,6 @@ financial_year <- function(date){
   
   if_else(month(date) >= 4,
           glue("{year(date)}/", "{substr(year(date) + 1, ", "3, 4)}"),
-          glue("{year(date) - 1}/", "{substr(year(date), ", "3, 4)}")
+          glue("{year(date) - 1}/", "{substr(year(date), ", "3, 4)}"))
   
 }
