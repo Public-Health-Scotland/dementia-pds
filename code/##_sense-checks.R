63c71ee5203de6185be9b29778280327d469f368
alicebyers5 <alice.byers@nhs.net>
2019-09-12 08:38:26 +0100

    Add list of expected health boards

--- code/##_sense-checks.R
+++ code/##_sense-checks.R
@@ -1,6 +1,21 @@
 
 # All boards are included in file
 
+hb <- c("A NHS Ayrshire & Arran",
+        "B NHS Borders",
+        "F NHS Fife",
+        "G NHS Greater Glasgow & Clyde",
+        "H NHS Highland",
+        "L NHS Lanarkshire",
+        "N NHS Grampian",
+        "R NHS Orkney",
+        "S NHS Lothian",
+        "T NHS Tayside",
+        "V NHS Forth Valley",
+        "W NHS Western Isles",
+        "Y NHS Dumfries & Galloway",
+        "Z NHS Shetland")
+
 # All FYs are included in file
 
 # All IJBs are included in file
