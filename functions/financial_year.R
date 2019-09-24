de840414af422ee712b3639f194746031fc43dbc
alicebyers5 <alice.byers@nhs.net>
2019-09-24 13:23:17 +0100

    Use paste instead of glue - issues with variable type

--- functions/financial_year.R
+++ functions/financial_year.R
@@ -2,7 +2,8 @@
 financial_year <- function(date){
   
   if_else(month(date) >= 4,
-          glue("{year(date)}/", "{substr(year(date) + 1, ", "3, 4)}"),
-          glue("{year(date) - 1}/", "{substr(year(date), ", "3, 4)}"))
+          paste0(year(date), "/", substr(year(date) + 1, 3, 4)),
+          paste0(year(date) - 1, "/", substr(year(date), 3, 4))
+          )
   
 }
