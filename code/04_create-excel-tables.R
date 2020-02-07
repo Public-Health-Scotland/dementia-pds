04466dd64e4d2cbf0226459bf18a9dfbd330dceb
alicebyers5 <alice.byers@nhs.net>
2020-02-07 14:50:14 +0000

    Restructure data for excel tables

---
+++ code/04_create-excel-tables.R
@@ -0,0 +1,107 @@
+#########################################################################
+# Name of file - 04_create-excel-tables.R
+# Data release - Annual Dementia PDS Publication
+# Original Authors - Alice Byers
+# Orginal Date - February 2020
+#
+# Written/run on - RStudio Server
+# Version of R - 3.5.1
+#
+# Description - Restructure data and save to tab in excel template
+#
+# Approximate run time - xx minutes
+#########################################################################
+
+
+### 1 - Load environment file ----
+
+source(here::here("code", "00_setup-environment.R"))
+
+
+### 2 - Load data ----
+
+pds <-
+  read_rds(here("data", 
+                      glue("{fy}-{substr(as.numeric(fy)+1, 3, 4)}/Q{qt}"),
+                      glue("{fy}-{qt}_final-data.rds")))
+
+
+### 3 - Restructure data ----
+
+pds %<>%
+  filter(fy %in% c("2016/17", "2017/18")) %>%
+  mutate(health_board = substring(health_board, 3),
+         ijb = substring(ijb, 11))
+
+
+excel_data <-
+  
+  bind_rows(
+    
+    # Health Board
+    pds %>% 
+      group_by(fy, category = "hb", category_split = health_board, ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)) %>%
+      arrange(fy, category, category_split),
+    
+    # Scotland Rows
+    pds %>% 
+      group_by(fy, category = "hb", category_split = "Scotland", ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)),
+    
+    # IJB
+    pds %>% 
+      group_by(fy, category = "ijb", category_split = ijb, ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)) %>%
+      arrange(fy, category, category_split),
+    
+    pds %>% 
+      group_by(fy, category = "ijb", category_split = "Scotland", ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)),
+    
+    # Age Group
+    pds %>% 
+      group_by(fy, category = "age", category_split = age_grp, ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)) %>%
+      arrange(fy, category, category_split),
+    
+    # Deprivation
+    pds %>% 
+      group_by(fy, category = "simd", category_split = simd, ldp) %>% 
+      summarise(referrals = sum(referrals, na.rm = TRUE)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = ldp, values_from = referrals) %>%
+      mutate_at(vars(complete:ongoing), ~ replace_na(., 0)) %>%
+      arrange(fy, category, category_split)
+    
+  ) %>%
+  
+  filter(!is.na(category_split)) %>%
+  mutate(referrals = reduce(select(., complete:ongoing), `+`),
+         rate = (complete + exempt) / (referrals - ongoing))
+
+
+### 4 - Save data to excel template ----
+
+write_csv(excel_data,
+          here("data", 
+               glue("{fy}-{substr(as.numeric(fy)+1, 3, 4)}/Q{qt}"),
+               glue("{fy}-{qt}_excel-data.csv")))
+
+
+### END OF SCRIPT ###
