ed8314aa1d9ce81335c3c01fc7ce4d88a5cd11f4
alicebyers5 <alice.byers@phs.scot>
2021-03-02 16:14:37 +0000

    Knit report

--- code/publication/02_knit-markdown.R
+++ code/publication/02_knit-markdown.R
@@ -11,18 +11,13 @@
 #########################################################################
 
 
-### 1 - Turn off kableExtra auto formatting ----
-
-# Causes issues when knitting kable to word
-options(kableExtra.auto_format = FALSE)
-
-
-### 2 - Load environment file ----
+### 0 - Load environment file and functions ----
 
 source(here::here("code", "00_setup-environment.R"))
+source(here("functions", "create_report.R"))
 
 
-### 3 - Knit summary ----
+### 1 - Knit summary ----
 
 render(
   input = here("publication", "markdown", "summary.Rmd"),
@@ -31,22 +26,9 @@ render(
 )
 
 
-### 4 - Knit report ----
+### 2 - Knit report ----
 
-params <- list(
-  rmd_filename    = here("publication", "markdown", "report.Rmd"),
-  cover_filename  = here("publication", "markdown", "templates", 
-                         "report-cover-page.docx"),
-  title           = "Dementia Post-Diagnostic Support",
-  subtitle        = paste("Local Delivery Plan Standard - Figures for",
-                          glue_collapse(fy_in_pub, sep = ", ", last = " and ")),
-  date1           = pub_date %>% format("%e %B %Y") %>% str_trim(),
-  date2           = pub_date %>% format("%d/%m/%Y"),
-  filename_out    = here("publication", "output", pub_date, 
-                         paste0(pub_date, "_report.docx"))
-)
-
-source(here("code", "publication", "compile.R"))
+create_report(pub_date, fy_in_pub)
 
 
 ### END OF SCRIPT ###
---
+++ functions/create_report.R
@@ -0,0 +1,63 @@
+
+create_report <- function(pub_date, fy_in_pub){
+  
+  `%>%` <- magrittr::`%>%`
+  
+  # Create Report and Add Table of Contents
+  rmarkdown::render(
+    here::here("publication", "markdown", "report.Rmd"), 
+    output_file = here::here("publication", "output", pub_date,
+                             paste0(pub_date, "_temp-report.docx")),
+    envir = new.env()
+  )
+  
+  officer::read_docx(here::here("publication", "output", pub_date,
+                                paste0(pub_date, "_temp-report.docx"))) %>%
+    officer::headers_replace_all_text("DATE HERE", 
+                                      format(pub_date, "%d/%m/%Y")) %>%
+    officer::cursor_reach(keyword = "Introduction") %>%
+    officer::body_add_toc(pos = "before") %>%
+    officer::body_add_par("Contents", pos = "before", 
+                          style = "Contents Header") %>%
+    officer::cursor_reach(keyword = "Introduction") %>%
+    officer::body_add_break(pos = "before") %>%
+    officer::cursor_reach(keyword = "Contents") %>%
+    officer::body_add_break(pos = "before") %>%
+    print(here::here("publication", "output", pub_date,
+                     paste0(pub_date, "_temp-report-2.docx")))
+  
+  # Cover Page
+  cover_page <- officer::read_docx(
+    here::here("publication", "markdown", "templates", 
+               "report-cover-page.docx")) %>%
+    officer::body_replace_all_text("Insert publication title here::here",
+                                   "Dementia Post-Diagnostic Support") %>%
+    officer::body_replace_all_text(
+      "Subtitle",
+      paste("Local Delivery Plan Standard - Figures for",
+            glue::glue_collapse(fy_in_pub, sep = ", ", last = " and "))) %>%
+    officer::body_replace_all_text(
+      "DD Month YYYY", 
+      stringr::str_trim(format(pub_date, "%e %B %Y"))) %>%
+    officer::body_replace_all_text("DATE HERE", format(pub_date, "%d/%m/%Y"))
+  
+  # Combine Cover and Report
+  cover_page %>%
+    officer::cursor_end() %>%
+    officer::body_add_break() %>%
+    officer::body_add_docx(
+      here::here("publication", "output", pub_date,
+                 paste0(pub_date, "_temp-report-2.docx"))) %>%
+    print(here::here("publication", "output", pub_date, 
+                     paste0(pub_date, "_report.docx")))
+  
+  # Remove Temporary Files
+  unlink(here::here("publication", "output", pub_date,
+                    paste0(pub_date, 
+                           c("_temp-report.docx",
+                             "_temp-report-2.docx"))))
+  
+}
+
+
+### END OF SCRIPT ###
