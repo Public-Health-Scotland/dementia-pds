3f725c4684875bad21ebdad1002ba171ad18190a
alicebyers5 <alice.byers@phs.scot>
2021-03-08 09:43:39 +0000

    Set plot margins

--- functions/ggplot_themes.R
+++ functions/ggplot_themes.R
@@ -1,4 +1,3 @@
-
 theme_dementia <- function(){
   theme_gray() +
     theme(panel.background = element_blank(),
@@ -11,61 +10,20 @@ theme_dementia <- function(){
                                       hjust = 0.5, vjust = 0.5),
           axis.text = element_text(size = 10),
           axis.line = element_line(),
-          legend.position = "none")
+          legend.position = "none",
+          plot.margin = margin(10, 10, 10, 10))
 }
 
 phs_colours <- c(
-  `phs-purple` = "#3F3685",
-  `phs-magenta` = "#9B4393",
-  `phs-blue` = "#0078D4",
-  `phs-green` = "#83BB26",
-  `phs-graphite` = "#948DA3",
-  `phs-teal` = "#1E7F84",
-  `phs-liberty` = "#6B5C85",
-  `phs-rust` = "#C73918"
+  `purple` = "#3F3685",
+  `magenta` = "#9B4393",
+  `blue` = "#0078D4",
+  `green` = "#83BB26",
+  `graphite` = "#948DA3",
+  `teal` = "#1E7F84",
+  `liberty` = "#6B5C85",
+  `rust` = "#C73918"
 )
-
-phs_cols <- function(...) {
-  cols <- c(...)
-  
-  if (is.null(cols))
-    return (phs_colours)
-  
-  phs_colours[cols]
-}
-
-phs_palettes <- list(
-  `main` = phs_cols("phs-purple", "phs-magenta", "phs-blue", "phs-green"),
-  `supporting` = phs_cols("phs-graphite", "phs-teal", "phs-liberty", "phs-rust")
-)
-
-phs_pal <- function(palette = "main", reverse = FALSE, ...) {
-  pal <- phs_palettes[[palette]]
-  
-  if (reverse) pal <- rev(pal)
-  
-  colorRampPalette(pal, ...)
-}
-
-scale_fill_phs <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
-  pal <- phs_pal(palette = palette, reverse = reverse)
-  
-  if (discrete) {
-    discrete_scale("fill", paste0("phs_", palette), palette = pal, ...)
-  } else {
-    scale_fill_gradientn(colours = pal(256), ...)
-  }
-}
-
-scale_colour_phs <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
-  pal <- phs_pal(palette = palette, reverse = reverse)
-  
-  if (discrete) {
-    discrete_scale("colour", paste0("phs_", palette), palette = pal, ...)
-  } else {
-    scale_color_gradientn(colours = pal(256), ...)
-  }
-}
 
 
 ### END OF SCRIPT ###
