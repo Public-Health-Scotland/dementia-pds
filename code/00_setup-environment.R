602c96f020539e53e6ae069e2b1ef7bdd700eeed
Jennifer Thom <jennifer.thom@phs.scot>
2023-11-16 08:48:09 +0000

    Update last day in reporting period(30-09-23)

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -16,7 +16,7 @@
 ### 0 - Manual Variable(s) - TO UPDATE 
 
 # UPDATE - Last day in reporting period (ddmmyyyy)
-end_date <- lubridate::dmy(30062023)
+end_date <- lubridate::dmy(30092023)
 
 ### 1 - Load packages ----
 
