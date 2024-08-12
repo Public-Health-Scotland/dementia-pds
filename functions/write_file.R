39ac144a872e0902c434e09965d089a1a2b2bec4
abramm01 <abram.mccormick@phs.scot>
2024-08-12 12:19:44 +0100

    Added `else if` statements and adjusted indentations.

--- functions/write_file.R
+++ functions/write_file.R
@@ -36,7 +36,7 @@ write_file <- function(data, path, ...) {
              file = path,
              ...
            )
-    )
+          )
     
     if (fs::file_info(path)$user == Sys.getenv("USER")) {
       # Set the correct permissions
@@ -45,29 +45,22 @@ write_file <- function(data, path, ...) {
     
     return(invisible(data))
   }
-  if (file.size(path) != 0) {
+  else if (file.size(path) != 0) {
   input <- menu(c("yes, overwrite the file (enter 0 to abort)"),title = cli::cli_alert_info("The file {.file {fs::path_file(path)}} already exists, are you sure you want to overwrite the file?"))
- 
-  if (input == 0){
-    cli::cli_abort(c(
-      "The file {.file {fs::path_file(path)}} already exists",
-      "x" = "File has not been overwritten. Re-run the section above and enter 1 in the console to overwrite file."
-    ))
-    
-  }
-if (input == 1){
-  valid_extensions <- c("rds", "csv")
+
+    if (input == 1){
+     valid_extensions <- c("rds", "csv")
   
-  ext <- fs::path_ext(path)
+      ext <- fs::path_ext(path)
   
-  if (!(ext %in% valid_extensions)) {
-    cli::cli_abort(c(
-      "x" = "Invalid extension: {.val {ext}}",
-      "i" = "{.fun read_file} supports {.val {valid_extensions}}"
-    ))
-  }
+      if (!(ext %in% valid_extensions)) {
+       cli::cli_abort(c(
+          "x" = "Invalid extension: {.val {ext}}",
+          "i" = "{.fun read_file} supports {.val {valid_extensions}}"
+       ))
+      }
   
-  switch(ext,
+      switch(ext,
          "rds" = readr::write_rds(
            x = data,
            file = path,
@@ -81,24 +74,24 @@ if (input == 1){
            file = path,
            ...
          )
-  )
+       )
   
-  if (fs::file_info(path)$user == Sys.getenv("USER")) {
-    # Set the correct permissions
-    fs::file_chmod(path = path, mode = "770")
-  }
-  cli::cli_alert_info("The file {.file {fs::path_file(path)}} has been overwritten")
-  return(invisible(data))
+      if (fs::file_info(path)$user == Sys.getenv("USER")) {
+       # Set the correct permissions
+       fs::file_chmod(path = path, mode = "770")
+       }
+      cli::cli_alert_info("The file {.file {fs::path_file(path)}} has been overwritten")
+      return(invisible(data))
+     }
   
-}else if (input == 0){
-  cli::cli_abort(c(
-    "The file {.file {fs::path_file(path)}} already exists",
-    "x" = "File has not been overwritten. Re-run the section above and enter 1 in the console to overwrite file."
+    else if (input == 0){
+     cli::cli_abort(c(
+     "The file {.file {fs::path_file(path)}} already exists",
+     "x" = "File has not been overwritten. Re-run the section above and enter 1 in the console to overwrite file."
   ))
 }
   }
+    }
   
-  }
-  
- }
+ 
 
