b5dcc8310e27d319b08be659d27d821111883a4b
abramm01 <abram.mccormick@phs.scot>
2024-08-12 15:09:40 +0100

    Adjusted section numbers so that write_file is always at the end of a section.

--- code/01_data-preparation.R
+++ code/01_data-preparation.R
@@ -175,7 +175,7 @@ dupes <-
   write_file(path = get_mi_data_path("dupe_data", ext = "csv", test_output = test_output))
 0 # this zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
-# Remove duplicate records
+### 7 - Remove duplicate records ---
 pds %<>%
   
   filter(dupe_keep == 1) %>%
@@ -188,9 +188,10 @@ pds %<>%
   select(-contains("dupe"))
 
 
-### 7 - Save data ---
+### 8 - Save data ---
 
 pds %>% 
 write_file(path = get_mi_data_path("clean_data", ext = "rds", test_output = test_output))
 0 # this zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
+
 ### END OF SCRIPT ###
