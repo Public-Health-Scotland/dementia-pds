83f663aa955f018493a6a51115ed96e0ea6a9211
abramm01 <abram.mccormick@phs.scot>
2024-08-12 15:11:49 +0100

    Amended code so that core of function is not being repeated.

--- functions/write_file.R
+++ functions/write_file.R
@@ -9,19 +9,19 @@
 # Description - Function to write to disk and set the correct permissions.  
 ################################################################################
 
-write_file <- function(data, path, ...) {
-  if(file.size(path) == 0){
+write_file <- function(data, path, input = 2, ...) {
+  if (file.size(path) != 0) {
+    input <- menu(c("yes, overwrite the file (enter 0 to abort)"), title = cli::cli_alert_info("The file {.file {fs::path_file(path)}} already exists, are you sure you want to overwrite the file?"))
+  }
+  if (file.size(path) == 0 | input == 1) {
     valid_extensions <- c("rds", "csv")
-    
     ext <- fs::path_ext(path)
-    
     if (!(ext %in% valid_extensions)) {
       cli::cli_abort(c(
         "x" = "Invalid extension: {.val {ext}}",
         "i" = "{.fun read_file} supports {.val {valid_extensions}}"
       ))
     }
-    
     switch(ext,
            "rds" = readr::write_rds(
              x = data,
@@ -36,62 +36,22 @@ write_file <- function(data, path, ...) {
              file = path,
              ...
            )
-          )
-    
+    )
     if (fs::file_info(path)$user == Sys.getenv("USER")) {
       # Set the correct permissions
       fs::file_chmod(path = path, mode = "770")
     }
-    
-    return(invisible(data))
   }
-  else if (file.size(path) != 0) {
-  input <- menu(c("yes, overwrite the file (enter 0 to abort)"),title = cli::cli_alert_info("The file {.file {fs::path_file(path)}} already exists, are you sure you want to overwrite the file?"))
-
-    if (input == 1){
-     valid_extensions <- c("rds", "csv")
-  
-      ext <- fs::path_ext(path)
-  
-      if (!(ext %in% valid_extensions)) {
-       cli::cli_abort(c(
-          "x" = "Invalid extension: {.val {ext}}",
-          "i" = "{.fun read_file} supports {.val {valid_extensions}}"
-       ))
-      }
-  
-      switch(ext,
-         "rds" = readr::write_rds(
-           x = data,
-           file = path,
-           compress = "xz",
-           version = 3L,
-           ...,
-           compression = 9L
-         ),
-         "csv" = readr::write_csv(
-           x = data,
-           file = path,
-           ...
-         )
-       )
-  
-      if (fs::file_info(path)$user == Sys.getenv("USER")) {
-       # Set the correct permissions
-       fs::file_chmod(path = path, mode = "770")
-       }
+    if (input == 1) {
       cli::cli_alert_info("The file {.file {fs::path_file(path)}} has been overwritten")
       return(invisible(data))
-     }
-  
-    else if (input == 0){
-     cli::cli_abort(c(
-     "The file {.file {fs::path_file(path)}} already exists",
-     "x" = "File has not been overwritten. Re-run the section above and enter 1 in the console to overwrite file."
-  ))
-}
-  }
     }
-  
- 
-
+    if (input == 2) {
+      return(invisible(data))
+  }
+   if (input == 0) {
+      cli::cli_abort(message =
+        "The file {.file {fs::path_file(path)}} already exists and has NOT been overwritten. Re-run the section above and enter 1 in the console to overwrite file."
+      )
+    }
+}
