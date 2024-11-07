08cd07ce5ec9db0d4682b6c6d61397e64de36345
abramm01 <abram.mccormick@phs.scot>
2024-11-07 10:38:49 +0000

    adds previous_end_date to setup

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -26,7 +26,8 @@
 test_output <- TRUE
 
 # UPDATE - Last day in reporting period (ddmmyyyy)
-end_date <- lubridate::dmy(30092024)
+end_date <- lubridate::dmy(30062024)
+previous_end_date <- lubridate::dmy(31032024)
 
 # UPDATE - Most recent Date of publication (ddmmyyyy)
 # Need this for set up of some folder structure
