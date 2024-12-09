3aa2a8e4a40b3da7a683af0ff5b9271d243f8039
abramm01 <abram.mccormick@phs.scot>
2024-12-09 15:54:31 +0000

    Reconfigures stacked 100% bar chart function for use with crosstalk filter

--- functions/plot_trend_prop.R
+++ functions/plot_trend_prop.R
@@ -1,21 +1,33 @@
 plot_trend_prop <- function(data){
   
-  plot <-  ggplot() +         
+  plot <- data %>%  ggplot(aes(x = fy, y = total_referrals, fill = age_grp, 
+                               text = paste0(geog, "<br>",
+                                             fy, "<br>", 
+                                             "Age Group: ", age_grp, "<br>",
+                                             "Total Referrals: ", total_referrals
+                                             ))) +    
+    
                
-          geom_col(aes(x = fy, y = total_referrals, fill = age_grp), position = position_fill(), data) +
-                         #text = paste0(geog, "<br>", "rate per 10,000 population: ", pop_rate_10000)
+          geom_col(position = position_fill()) +
     
-             scale_y_continuous(labels = scales::percent_format()) +
+          facet_wrap(vars(geog), nrow = 1) +
     
-     facet_wrap(vars(geog), nrow = 1, ncol = 1) +
+         scale_y_continuous(labels = scales::percent_format()) +
     
-    labs(title = "Proportion of referrals by Age Group and Financial Year ",
-         x = "",
-         y = "") 
-    
-     theme(legend.position = "right") 
   
-  ggplotly(plot) %>% #, tooltip = "text") %>%
+    labs(title = "Proportion of referrals by Age Group and Financial Year ",
+         x = "Financial Year of Diagnosis",
+         y = "",
+         fill = NULL) +
+    
+     theme(strip.background = element_blank(),
+         strip.text.x = element_blank(),
+         legend.position = "none") + 
+    
+    theme(panel.spacing = unit(2000, "pt"))
+
+  
+  ggplotly(plot, tooltip = "text") %>%
      
     config(displayModeBar = TRUE,
            modeBarButtonsToRemove = list('select2d', 'lasso2d', 'zoomIn2d', 
@@ -24,12 +36,32 @@ plot_trend_prop <- function(data){
                                          'hoverCompareCartesian', 
                                          'hoverClosestCartesian'), 
            displaylogo = F, editable = F) %>%
-     #layout(legend = list(orientation = "v") %>% 
+    # layout(legend = list(orientation = "v")) %>% 
                           #, x = 0.5 , y = -0.4,
                            #xanchor = "right", yanchor = "right")) %>% 
-     layout(margin = list(b = 30, t = 30) # to avoid labels getting cut out
+     layout(margin = list(b = 50, t = 30) # to avoid labels getting cut out
           ) 
  
 }
 
+plot_trend_prop_legend <- function(data) { 
+ legend <- data %>% ggplot(aes(x = fy, y = total_referrals, fill = age_grp))+
+   geom_col(width = 0)+
+   
+  labs(fill = "Age Group") +
+   
+      theme(axis.title = element_blank(),
+         axis.text = element_blank(),
+         axis.ticks = element_blank(),
+         legend.position = c(0.4, 0.5), # move the legend to the center
+         panel.grid = element_blank(),
+         panel.border = element_rect(colour = "white", fill='white', size=1)
+   )
+   
+ legend_plotly<-ggplotly(legend, tooltip = NULL)
+   
+ config(legend_plotly, staticPlot = TRUE) %>%
+ layout(legend = list(orientation = "v",
+          x = 0, y = 0.5))  
+}
 
