6811b82f6a7268b3b47d0d3e9ac8b84526115ad1
abramm01 <abram.mccormick@phs.scot>
2025-05-29 14:02:17 +0000

    update to 2024/25 Q4

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -26,8 +26,8 @@
 test_output <- TRUE
 
 # UPDATE - Last day in reporting period (ddmmyyyy)
-end_date <- lubridate::dmy(31122024)
-previous_end_date <- lubridate::dmy(30092024)
+end_date <- lubridate::dmy(31032025)
+previous_end_date <- lubridate::dmy(31122024)
 
 # UPDATE - Most recent Date of publication (ddmmyyyy)
 # Need this for set up of some folder structure
