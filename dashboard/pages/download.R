20275ae091a4b79ac4b9b61bc291c740e1eb4c33
abramm01 <abram.mccormick@phs.scot>
2025-08-18 15:31:22 +0000

    makes revision and provisional note on csv dynamic depending on what years are selected

--- dashboard/pages/download.R
+++ dashboard/pages/download.R
@@ -190,60 +190,77 @@ output$table_download <- DT::renderDataTable({
 }
 )
 
-# download button----
+# download button data----
+
+csv_data<- reactive({download_data_filtered() %>%
+  mutate(financial_year = case_when(
+    # changes superscripts to inline text as csv does not support superscripts
+    financial_year == provisional_year_sup ~paste0(provisional_year,"P"),
+    financial_year == revised_year_sup ~paste0(revised_year,"R"),
+    financial_year == extra_referrals_year_sup ~paste0(extra_referrals_year,"P"),
+    TRUE ~financial_year)) %>%
+  #adding (R) to indicate where revisions were made in 2025 publication due to Aberdeen City and incorrect formula being used
+  #REMOVE this section from 2026 onward----
+mutate(measure = 
+         if(input$download == "download_data_scotland"){
+           case_when(
+             (financial_year == "2020/21" & gender == "All" & age_group != "90+" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
+             (financial_year == "2020/21" & measure == "LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds")
+             ~"LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds(R)",
+             (financial_year == "2020/21" & gender == "All" & age_group != "59 and Under" & age_group != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
+             ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
+             (financial_year == "2020/21" & gender == "All" & deprivation_quintile != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
+             ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
+             (financial_year == "2019/20" & gender == "All" & age_group != "59 and Under" & age_group != "60 to 64" & age_group != "70 to 74" & age_group != "90+" & age_group != "All" & measure == "number of people referred to PDS") 
+             ~"number of people referred to PDS(R)",
+             (financial_year == "2019/20" & gender == "All" & deprivation_quintile != "All" & measure == "number of people referred to PDS") 
+             ~"number of people referred to PDS(R)",
+             (financial_year == "2019/20" & gender == "All" & age_group != "59 and Under" & age_group != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
+             ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
+             (financial_year == "2019/20" & gender == "All" & deprivation_quintile != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
+             ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
+             TRUE ~measure
+           )
+         }else if(input$download == "download_data_hb"){ 
+           case_when(
+             (geography == "NHS Grampian" & financial_year == "2020/21" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
+             (geography == "NHS Grampian" & financial_year == "2020/21" & measure == "LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds")
+             ~"LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds(R)",
+             TRUE ~measure
+           )
+         }else{
+           case_when(
+             (geography == "Aberdeen City" & financial_year == "2020/21" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
+             TRUE ~measure
+           )
+         }
+)
+})
+
 output$downloadData <- downloadHandler(
+  
   filename = paste0("pds_data_as_at_", end_date, ".csv"),
   content = function(file) {
-    write.csv(download_data_filtered() %>%
-                mutate(financial_year = case_when(
-                  financial_year == provisional_year_sup ~paste0(provisional_year,"P"),
-                  financial_year == revised_year_sup ~paste0(revised_year,"R"),
-                  financial_year == extra_referrals_year_sup ~paste0(extra_referrals_year,"P"),
-                                                    TRUE ~financial_year)) %>%
-                #adding (R) to indicate where revisions were made in 2025 publication due to Aberdeen City and incorrect formula being used
-                #REMOVE this section from 2025 onward----
-              mutate(measure = 
-                if(input$download == "download_data_scotland"){
-                  case_when(
-                    (financial_year == "2020/21" & gender == "All" & age_group != "90+" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
-                    (financial_year == "2020/21" & measure == "LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds")
-                    ~"LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds(R)",
-                    (financial_year == "2020/21" & gender == "All" & age_group != "59 and Under" & age_group != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
-                    ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
-                    (financial_year == "2020/21" & gender == "All" & deprivation_quintile != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
-                    ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
-                    (financial_year == "2019/20" & gender == "All" & age_group != "59 and Under" & age_group != "60 to 64" & age_group != "70 to 74" & age_group != "90+" & age_group != "All" & measure == "number of people referred to PDS") 
-                                                   ~"number of people referred to PDS(R)",
-                    (financial_year == "2019/20" & gender == "All" & deprivation_quintile != "All" & measure == "number of people referred to PDS") 
-                    ~"number of people referred to PDS(R)",
-                    (financial_year == "2019/20" & gender == "All" & age_group != "59 and Under" & age_group != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
-                    ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
-                    (financial_year == "2019/20" & gender == "All" & deprivation_quintile != "All" & measure == "LDP standard part 2 - percentage of LDP standard achieved")
-                    ~"LDP standard part 2 - percentage of LDP standard achieved(R)",
-                    TRUE ~measure
-                         )
-                }else if(input$download == "download_data_hb"){ 
-                  case_when(
-                    (geography == "NHS Grampian" & financial_year == "2020/21" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
-                    (geography == "NHS Grampian" & financial_year == "2020/21" & measure == "LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds")
-                    ~"LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds(R)",
-                     TRUE ~measure
-                  )
-                }else{
-                  case_when(
-                    (geography == "Aberdeen City" & financial_year == "2020/21" & measure == "number of people referred to PDS") ~"number of people referred to PDS(R)",
-                    TRUE ~measure
-                    )
-                  }
+    write.csv(csv_data() %>%  
+                rbind(
+                  if(any(download_data_filtered()$financial_year %in% c(extra_referrals_year_sup, provisional_year_sup)) 
+                  ){
+                    c("Note: P indicates data is provisional. Please see dashboard for further information.",rep("",6))
+                  }else{NULL}
                 ) %>% 
-                rbind(c("Note: P indicates data is provisional. Please see dashboard for further information.",rep("",6))
-                      
-                ) %>% 
-                rbind(c("Note: R indicates data has been revised. Please see dashboard for further information.",rep("",6))
+                rbind(
+                  if(any(download_data_filtered()$financial_year == revised_year_sup) 
+                     #REMOVE the following 5 lines from 2026 onward----
+                   | any(csv_data()$measure == "number of people referred to PDS(R)") |
+                     any(csv_data()$measure == "LDP standard part 2 - percentage of LDP standard achieved(R)") |
+                     any(csv_data()$measure == "number of people referred to PDS(R)") |
+                     any(csv_data()$measure == "LDP standard part 2 - percentage of LDP standard achieved(R)") |
+                     any(csv_data()$measure == "LDP standard part 1 - percentage of estimated number of people diagnosed with dementia referred to pds(R)")
+                  ){
+                    c("Note: R indicates data has been revised. Please see dashboard for further information.",rep("",6))
+                  }else{NULL}
                 ),
-               file, row.names = FALSE)
+              file, row.names = FALSE)
   }
 )
-
-
 
