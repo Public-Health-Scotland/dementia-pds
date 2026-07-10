f495e3940da71e9ee246220a7360cdc066ae56ad
LucyEmma22 <lucyemma02@gmail.com>
2026-07-10 11:52:59 +0000

    Remove sourcing of 1_PREPARE_DATA.R and 2_CREATE_ADMIN_CREDENTIALS.R from 3_DEPLOY_APP.R

--- dashboard/deploy_app/3_DEPLOY_APP.R
+++ dashboard/deploy_app/3_DEPLOY_APP.R
@@ -1,31 +1,33 @@
 ################################################################################.
-# SOURCE THIS SCRIPT TO DEPLOY APP
-# VARIABLES TO UPDATE: "token", "secret", "filepath", "appName", "password_protect_deploy"
-# "token" & "secret" ARE LOCATED IN "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-################################################################################.
-
-################################################################################.
 # Name of file - 3_DEPLOY_APP.R
 # Original Authors - Lucy Binsted
 # Original Date - Dec 2025
 # Written/run on - RStudio Server
 # Version of R - 4.4.2
-# Description - Deploy R shiny dashboard. 
+# Description - SOURCE THIS SCRIPT TO DEPLOY APP 
 ################################################################################.
 
+#===============================================================================.
 # Manual Variables - TO UPDATE ----
-source("//conf/dementia/A&I/Outputs/dashboard/passwords.R")
-token <- token # Defined in "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-secret <- secret # Defined in "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-filepath <- "/conf/dementia/A&I/Analysts/Lucy/dementia-pds/dashboard" # Absolute path to the folder the app is being deployed from
-appName <- "phs-dementiapds2025-app" # Name of the app
-password_protect_deploy <- "off" # "on" for pre-release, "off" for general release
-  
-# Update data and admin credentials
-source(here::here("dashboard", "deploy_app", "1_PREPARE_DATA.R"))
-source(here::here("dashboard", "deploy_app", "2_CREATE_ADMIN_CREDENTIALS.R"))
 
-# Overwrite password_protect and AUTH_ENABLED set in 2_CREATE_ADMIN_CREDENTIALS.R
+# MANUAL UPDATE: Update `user`, `password`, `token` and `secret` in `/conf/dementia/A&I/Outputs/dashboard/passwords.R`
+# MANUAL UPDATE: Run 1_PREPARE_DATA.R
+# MANUAL UPDATE: Run 2_CREATE_ADMIN_CREDENTIALS.R
+
+# MANUAL UPDATE: Absolute path to the folder the app is being deployed from. Change analyst name.
+filepath <- "/conf/dementia/A&I/Analysts/Lucy/dementia-pds/dashboard" 
+
+# MANUAL UPDATE: Name of the app. Change year.
+appName <- "phs-dementiapds2026-app" 
+
+# MANUAL UPDATE: "on" for pre-release, "off" for general release.
+password_protect_deploy <- "on" 
+#===============================================================================.
+
+# Read in passwords from external file
+source("//conf/dementia/A&I/Outputs/dashboard/passwords.R")
+
+# Save whether password protect is on or off
 password_protect <- password_protect_deploy
 if (password_protect == "on"){
   AUTH_ENABLED <- TRUE
@@ -37,12 +39,16 @@ if (password_protect == "on"){
 save(AUTH_ENABLED, file = here::here("dashboard", "data", "AUTH_ENABLED.RData"))
 
 # Ask user to check that end_date, pub_date and last_pub_date have been updated
+source(here::here("code", "publication", "00_setup-pub-environment.R")) # For checking setup is updated
+load(here::here("dashboard", "data", "dashboard_variables.RData")) # For checking dashboard data is updated
+fy <- extract_fin_year(end_date) %>% substr(1, 4)
+qt <- quarter(end_date, fiscal_start = 4)   
+
 check_msg1 <- paste0("Please check the data being used is correct.",
                      "\n\nYear: ", fy,
                      "\nQuarter: ", qt,
-                     #"\nLast day in reporting period: ", end_date,
-                     #"\nDate of publication: ", pub_date,
-                     #"\nDate of last publication: ", last_pub_date,
+                     "\nDate of publication: ", pub_date,
+                     "\nDate of last publication: ", last_pub_date,
                      "\nVersion: ", ifelse(test_output == FALSE, "Finalised", "Test"))
 stop_msg1 <- "App deployment cancelled by user. Update 'end_date', 'pub_date', and 'last_pub_date' in 'dementia-pds/code/publication/00_setup-pub-environment.R'"
 continue_msg <- "Continuing app deployment..."
@@ -52,28 +58,30 @@ check_msg2 <- paste0("Password protect is ", password_protect, ".")
 stop_msg2 <- "App deployment cancelled by user. Update 'password_protect_deploy'"
 
 # Option 1: RStudio pop-up message
-# library(rstudioapi)
-# if (!rstudioapi::showQuestion(title = "Publication Dates", message = paste0(check_msg1,"\n\nClick OK to continue app deployment."))) {
-#   stop(stop_msg1)} else {message(continue_msg)}
-# if (!rstudioapi::showQuestion(title = "Password Protect", message = paste0(check_msg2, "\n\nClick OK to continue app deployment."))) {
-#   stop(stop_msg2)} else {message(continue_msg)}
+library(rstudioapi)
+if (!rstudioapi::showQuestion(title = "Publication Dates", message = paste0(check_msg1,"\n\nClick OK to continue app deployment."))) {
+  stop(stop_msg1)} else {message(continue_msg)}
+if (!rstudioapi::showQuestion(title = "Password Protect", message = paste0(check_msg2, "\n\nClick OK to continue app deployment."))) {
+  stop(stop_msg2)} else {message(continue_msg)}
 
 # Option 2: Message in console
-confirm_deployment <- function() {
-  message(paste0(check_msg1, "\n\nType 'ok' to continue app deployment."))
-  ans1 <- invisible(readline())
-  if (ans1 != "ok") {stop(stop_msg1, call. = FALSE)} else {
-    message(paste0(check_msg2, "\n\nType 'ok' to continue app deployment."))
-    ans2 <- invisible(readline())
-    if (ans2 != "ok") {stop(stop_msg2, call. = FALSE)} else {
-      message(continue_msg)}}}
-confirm_deployment()
+# confirm_deployment <- function() {
+#   message(paste0(check_msg1, "\n\nType 'ok' to continue app deployment."))
+#   ans1 <- invisible(readline())
+#   if (ans1 != "ok") {stop(stop_msg1, call. = FALSE)} else {
+#     message(paste0(check_msg2, "\n\nType 'ok' to continue app deployment."))
+#     ans2 <- invisible(readline())
+#     if (ans2 != "ok") {stop(stop_msg2, call. = FALSE)} else {
+#       message(continue_msg)}}}
+# confirm_deployment()
 
 # Connecting to the shiny.io account.
 if (!require('remotes')) install.packages('remotes'); library('remotes')
 if (!require('rsconnect')) install.packages('rsconnect'); library('rsconnect')
 options(rsconnect.packrat = TRUE)
 rsconnect::setAccountInfo(name='scotland', token=token, secret=secret)
+
+# Deploy the app
 rsconnect::deployApp(filepath, appName = appName)
 
 # Copy data used in the dashboard to stats drive
@@ -81,7 +89,7 @@ files <- list.files(
   here::here("dashboard", "data"),
   full.names = TRUE)
 
-dest_dir <- file.path("conf", "dementia", "A&I", "Outputs", "dashboard", "data", pub_date)
+dest_dir <- file.path("/conf", "dementia", "A&I", "Outputs", "dashboard", "data", pub_date)
 
 dir.create(dest_dir, recursive = TRUE, showWarnings = FALSE)
 
