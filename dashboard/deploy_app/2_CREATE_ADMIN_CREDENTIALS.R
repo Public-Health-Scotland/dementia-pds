4ae167854b17e166f4f1adeffd88ae12e10790fd
LucyEmma22 <lucyemma02@gmail.com>
2026-07-10 11:51:54 +0000

    Update notes in 2_CREATE_ADMIN_CREDENTIALS.R

--- dashboard/deploy_app/2_CREATE_ADMIN_CREDENTIALS.R
+++ dashboard/deploy_app/2_CREATE_ADMIN_CREDENTIALS.R
@@ -1,23 +1,23 @@
 ################################################################################.
-# SOURCE THIS SCRIPT TO UPDATE ADMIN CREDENTIALS FOR FOR DASHBOARD
-# VARIABLES TO UPDATE: "user", "password", "password_protect"
-# "user" & "password" ARE LOCATED IN "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-################################################################################.
-
-################################################################################.
 # Name of file - 2_CREATE_ADMIN_CREDENTIALS.R
 # Original Authors - Lucy Binsted
 # Original Date - Dec 2025
 # Written/run on - RStudio Server
 # Version of R - 4.4.2
-# Description - Create credentials for password protect for R shiny dashboard. 
+# Description - SOURCE THIS SCRIPT TO UPDATE ADMIN CREDENTIALS FOR FOR DASHBOARD.
 ################################################################################.
 
+#===============================================================================.
 # Manual Variables - TO UPDATE ----
+
+# MANUAL UPDATE: Set `user` and `password` in `/conf/dementia/A&I/Outputs/dashboard/passwords.R`
+
+# MANUAL UPDATE: "on" or "off"
+password_protect <- "on" 
+#===============================================================================.
+
+# Sets `user` and `password`
 source("//conf/dementia/A&I/Outputs/dashboard/passwords.R")
-user <- user # Defined in "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-password <- password # Defined in "//conf/dementia/A&I/Outputs/dashboard/passwords.R"
-password_protect <- "off" # "on" or "off"
 
 # Create data folder if it does not exist
 if (!dir.exists(here::here("dashboard/data"))) {dir.create(here::here("dashboard/data"))}
