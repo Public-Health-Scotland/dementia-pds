de8edd9afb8eff18f052585db70d0c2ab3ebe1a0
LucyEmma22 <lucyemma02@gmail.com>
2026-07-27 20:28:00 +0000

    Update formatting

--- code/01_data-preparation.R
+++ code/01_data-preparation.R
@@ -1,4 +1,4 @@
-################################################################################
+################################################################################.
 # Name of file - 01_data-preparation.R
 # Data release - Dementia PDS Quarterly Management Reports
 # Original Authors - Alice Byers
@@ -10,20 +10,22 @@
 # Version of R - 4.1.2
 #
 # Description - Clean collated file and remove duplicates.
-################################################################################
+################################################################################.
 
-
+################################################################################.
 ### 1 - Load environment file ----
+################################################################################.
 
 source(here::here("code", "00_setup-environment.R"))
 
-
+################################################################################.
 ### 2 - Read and clean collated file ----
+################################################################################.
 
-pds <- read_csv(get_national_data_path(fy, qt),
-                col_types = cols(.default = "c")
-                )%>%
-  
+pds <- read_csv(
+  get_national_data_path(fy, qt),
+  col_types = cols(.default = "c")
+) %>%
   clean_names() %>%
   
   # Convert dates from character to date format
@@ -45,7 +47,9 @@ pds <- read_csv(get_national_data_path(fy, qt),
 pds <- pds %>%
   filter(!chi_number %in% (pds %>% filter(health_board == "V NHS Forth Valley", ijb == "S37000033 Perth and Kinross"))$chi_number)
 
+################################################################################.
 ### 3 - Add finalised data ----
+################################################################################.
 
 finalised_years <- 
   list.files(get_finalised_data_dir()) %>% 
@@ -53,12 +57,10 @@ finalised_years <-
   str_replace("-", "/")
 
 pds <-
-  
   # Read in and bind all previous finalised years data
   list.files(get_finalised_data_dir(), full.names = TRUE) %>%
   map(read_rds) %>%
   reduce(bind_rows) %>%
-  
   # Add to latest submission data
   bind_rows(
     pds %>% 
@@ -67,25 +69,33 @@ pds <-
                finalised_years)
   )
 
-
+################################################################################.
 ### 4 - Save out error and query summary ----
+################################################################################.
 
-#queries/errors for all years
+# Queries/errors for all years
 q_err <- pds %>%
-  mutate(health_board = if_else(is.na(health_board),
-                                "Missing",
-                                substring(health_board, 3))) %>%
-  mutate(fy = extract_fin_year(dementia_diagnosis_confirmed_date)) %>%
+  mutate(
+    health_board = if_else(
+      is.na(health_board),
+      "Missing",
+      substring(health_board, 3)),
+    fy = extract_fin_year(dementia_diagnosis_confirmed_date)
+  ) %>%
   group_by(fy, health_board, ijb) %>%
-  summarise(total_q_errors     = sum(as.integer(error_flag)),
-            records          = n(),
-            .groups = "drop") %>%
+  summarise(
+    total_q_errors = sum(as.integer(error_flag)),
+    records = n(),
+    .groups = "drop") %>%
   group_by(fy) %>%
-  group_modify(
-    ~ bind_rows(.x, summarise(.x,
-                              health_board = "Scotland",
-                              ijb = "Scotland",
-                              across(c(total_q_errors, records), sum)))
+  group_modify(~ bind_rows(
+    .x, 
+    summarise(
+      .x,
+      health_board = "Scotland",
+      ijb = "Scotland",
+      across(c(total_q_errors, records), sum)
+    ))
   ) %>%
   ungroup() %>%
   arrange(fy, health_board, ijb) %T>%
@@ -100,23 +110,29 @@ q_err <- pds %>%
       create_dir = TRUE))
 #0 # This zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
-#queries for 2021/22 onward
-query <- pds %>% filter(dementia_diagnosis_confirmed_date >= dmy(01042021)) %>% 
-  mutate(health_board = if_else(is.na(health_board),
-                                "Missing",
-                                substring(health_board, 3))) %>%
-  mutate(fy = extract_fin_year(dementia_diagnosis_confirmed_date)) %>%
+# Queries for 2021/22 onward
+query <- pds %>% 
+  filter(dementia_diagnosis_confirmed_date >= dmy(01042021)) %>% 
+  mutate(
+    health_board = if_else(
+      is.na(health_board),
+      "Missing",
+      substring(health_board, 3)),
+    fy = extract_fin_year(dementia_diagnosis_confirmed_date)) %>%
   group_by(fy, health_board, ijb) %>%
-  summarise(total_queries     = sum(as.integer(q_flag)),
-            records          = n(),
-            .groups = "drop") %>%
+  summarise(
+    total_queries = sum(as.integer(q_flag)),
+    records = n(),
+    .groups = "drop") %>%
   group_by(fy) %>%
-  group_modify(
-    ~ bind_rows(.x, summarise(.x,
-                              health_board = "Scotland",
-                              ijb = "Scotland",
-                              across(c(total_queries, records), sum)))
-  ) %>%
+  group_modify(~ bind_rows(
+    .x, 
+    summarise(
+      .x,
+      health_board = "Scotland",
+      ijb = "Scotland",
+      across(c(total_queries, records), sum))
+  )) %>%
   ungroup() %>%
   arrange(fy, health_board, ijb) %T>%
   write_file(
@@ -130,23 +146,27 @@ query <- pds %>% filter(dementia_diagnosis_confirmed_date >= dmy(01042021)) %>%
       create_dir = TRUE))
 #0 # This zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
-#errors for 2021/23 onward
+# Errors for 2021/23 onward
 err <- pds %>% filter(dementia_diagnosis_confirmed_date >= dmy(01042021)) %>% 
-  mutate(health_board = if_else(is.na(health_board),
-                                "Missing",
-                                substring(health_board, 3))) %>%
-  mutate(fy = extract_fin_year(dementia_diagnosis_confirmed_date)) %>%
+  mutate(
+    health_board = if_else(
+      is.na(health_board),
+      "Missing",
+      substring(health_board, 3)),
+    fy = extract_fin_year(dementia_diagnosis_confirmed_date)) %>%
   group_by(fy, health_board, ijb) %>%
-  summarise(total_errors     = sum(as.integer(e_flag)),
-            records          = n(),
-            .groups = "drop") %>%
+  summarise(
+    total_errors = sum(as.integer(e_flag)),
+    records = n(),
+    .groups = "drop") %>%
   group_by(fy) %>%
-  group_modify(
-    ~ bind_rows(.x, summarise(.x,
-                              health_board = "Scotland",
-                              ijb = "Scotland",
-                              across(c(total_errors, records), sum)))
-  ) %>%
+  group_modify(~ bind_rows(
+    .x, summarise(
+      .x,
+      health_board = "Scotland",
+      ijb = "Scotland",
+      across(c(total_errors, records), sum))
+  )) %>%
   ungroup() %>%
   arrange(fy, health_board, ijb) %T>%
   write_file(
@@ -160,92 +180,87 @@ err <- pds %>% filter(dementia_diagnosis_confirmed_date >= dmy(01042021)) %>%
       create_dir = TRUE))
 #0 # This zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
+################################################################################.
 ### 5 - Recode Lanarkshire IJB records ----
+################################################################################.
 
 pds %<>%
-  mutate(health_board = 
-           case_when(
-             str_detect(ijb, "S37000035|S37000028") ~ "L NHS Lanarkshire",
-             TRUE ~ health_board
-           ))
+  mutate(
+    health_board = case_when(
+      str_detect(ijb, "S37000035|S37000028") ~ "L NHS Lanarkshire",
+      TRUE ~ health_board
+    )
+  )
 
-
+################################################################################.
 ### 6 - Remove duplicate records ----
+################################################################################.
 
 pds %<>%
-  
   group_by(chi_number) %>%
-  
-  # Add duplicate flag
-  mutate(dupe = if_else(!is.na(chi_number) & n() > 1, 1, 0)) %>%
-  
-  # Add flag for GGC/H duplicates
-  mutate(ggc_h_dupe = 
-           if_else(!is.na(chi_number) & n() > 1 &
-                     all(c("G NHS Greater Glasgow & Clyde",
-                           "H NHS Highland") %in% 
-                           health_board),
-                   1,
-                   0)) %>% 
+  mutate(
+    # Add duplicate flag
+    dupe = if_else(!is.na(chi_number) & n() > 1, 1, 0),
+    # Add flag for GGC/H duplicates
+    ggc_h_dupe = if_else(
+      !is.na(chi_number) & 
+        n() > 1 &
+        all(c("G NHS Greater Glasgow & Clyde", "H NHS Highland") %in% health_board),
+      1, 0
+    )
+  ) %>%
   
   # Add priority flag for duplicates to keep
   # Earliest diagnosis date, Term reason 04, Earliest contact date
-  mutate(dupe_keep = 
-           
-           case_when(
-             
-             # Keep GGC record where GGC/Highland duplicate record
-             
-             ggc_h_dupe == 1 & str_detect(health_board, "^G") ~ 1,
-             ggc_h_dupe == 1 & !str_detect(health_board, "^G") ~ 0,
-             
-             # Keep earliest diag date where different
-             
-             dupe == 1 &
-               n_distinct(dementia_diagnosis_confirmed_date) > 1 &
-               dementia_diagnosis_confirmed_date == 
-               min(dementia_diagnosis_confirmed_date, na.rm = TRUE) ~ 1,
-             
-             dupe == 1 &
-               n_distinct(dementia_diagnosis_confirmed_date) > 1 &
-               (!(dementia_diagnosis_confirmed_date == 
-                    min(dementia_diagnosis_confirmed_date, na.rm = TRUE)) |
-                  is.na(dementia_diagnosis_confirmed_date)) ~ 0,
-             
-             # Keep termination reason 04 Moved to different Health Board
-             
-             dupe == 1 &
-               n_distinct(termination_or_transition_reason) > 1 &
-               str_detect(termination_or_transition_reason, "^04") ~ 1,
-             
-             dupe == 1 &
-               n_distinct(termination_or_transition_reason) > 1 &
-               any(str_detect(termination_or_transition_reason, "^04")) ~ 0,
-             
-             # Keep earliest contact date
-             
-             dupe == 1 &
-               n_distinct(date_of_initial_first_contact) > 1 &
-               date_of_initial_first_contact == 
-               min(date_of_initial_first_contact, na.rm = TRUE) ~ 1,
-             
-             dupe == 1 &
-               n_distinct(date_of_initial_first_contact) > 1 &
-               (!(date_of_initial_first_contact == 
-                    min(date_of_initial_first_contact, na.rm = TRUE)) |
-                  is.na(date_of_initial_first_contact)) ~ 0,
-             
-             # Keep all records that aren't duplicates
-             dupe != 1 ~ 1
-             
-           )) %>%
-  
+  mutate(
+    dupe_keep = case_when(
+      
+      # Keep GGC record where GGC/Highland duplicate record
+      ggc_h_dupe == 1 & str_detect(health_board, "^G") ~ 1,
+      
+      ggc_h_dupe == 1 & !str_detect(health_board, "^G") ~ 0,
+      
+      # Keep earliest diag date where different
+      dupe == 1 &
+        n_distinct(dementia_diagnosis_confirmed_date) > 1 &
+        dementia_diagnosis_confirmed_date == 
+        min(dementia_diagnosis_confirmed_date, na.rm = TRUE) ~ 1,
+      
+      dupe == 1 &
+        n_distinct(dementia_diagnosis_confirmed_date) > 1 &
+        (!(dementia_diagnosis_confirmed_date == 
+             min(dementia_diagnosis_confirmed_date, na.rm = TRUE)) |
+           is.na(dementia_diagnosis_confirmed_date)) ~ 0,
+      
+      # Keep termination reason 04 Moved to different Health Board
+      dupe == 1 &
+        n_distinct(termination_or_transition_reason) > 1 &
+        str_detect(termination_or_transition_reason, "^04") ~ 1,
+      
+      dupe == 1 &
+        n_distinct(termination_or_transition_reason) > 1 &
+        any(str_detect(termination_or_transition_reason, "^04")) ~ 0,
+      
+      # Keep earliest contact date
+      dupe == 1 &
+        n_distinct(date_of_initial_first_contact) > 1 &
+        date_of_initial_first_contact == 
+        min(date_of_initial_first_contact, na.rm = TRUE) ~ 1,
+      
+      dupe == 1 &
+        n_distinct(date_of_initial_first_contact) > 1 &
+        (!(date_of_initial_first_contact == 
+             min(date_of_initial_first_contact, na.rm = TRUE)) |
+           is.na(date_of_initial_first_contact)) ~ 0,
+      
+      # Keep all records that aren't duplicates
+      dupe != 1 ~ 1
+      
+    )) %>%
   ungroup()
 
-
 # Save duplicate records
-dupes <- 
-  pds %>% 
+dupes <- pds %>% 
   filter(dupe == 1) %T>%
   write_file(
     path = get_mi_data_path(
@@ -258,20 +273,30 @@ dupes <-
       create_dir = TRUE))
 #0 # This zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
+################################################################################.
 ### 7 - Remove duplicate records ----
+################################################################################.
+
 pds %<>%
-  
   filter(dupe_keep == 1) %>%
-  
   # Recode GGC duplicate as Argyll & Bute activity
-  mutate(health_board = 
-           if_else(ggc_h_dupe == 1, "H NHS Highland", health_board),
-         ijb = if_else(ggc_h_dupe == 1, "S37000004 Argyll and Bute", ijb)) %>%
-  
+  mutate(
+    health_board = if_else(
+      ggc_h_dupe == 1, 
+      "H NHS Highland", 
+      health_board
+    ),
+    ijb = if_else(
+      ggc_h_dupe == 1, 
+      "S37000004 Argyll and Bute", 
+      ijb
+    )
+  ) %>%
   select(-contains("dupe"))
 
-
+################################################################################.
 ### 8 - Save data ----
+################################################################################.
 
 pds %>% 
   write_file(
@@ -285,4 +310,4 @@ pds %>%
       create_dir = TRUE))
 #0 # This zero stops script from running IF write_file is overwriting an existing file, re-run the section without this line and enter 1 in the console, when prompted, to overwrite file.
 
-### END OF SCRIPT ###
+################################ END OF SCRIPT #################################.
